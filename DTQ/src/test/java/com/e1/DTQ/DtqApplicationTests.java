b6bbd0f03859021cf97c0318a42d57e07c722bce
Ewan Melibari <ewanhasan2004@gmail.com>
2026-03-03 01:36:59 +0300

    Add files via upload

---
+++ DTQ/src/test/java/com/e1/DTQ/DtqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e1.DTQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
