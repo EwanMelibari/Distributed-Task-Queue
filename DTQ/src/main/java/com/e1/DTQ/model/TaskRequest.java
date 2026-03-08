837e300d2ba19328c56e759d80fdc872eb3f6822
EwanMelibari <ewanhasan2004@gmail.com>
2026-03-08 22:41:47 +0300

    Refactor TaskRequest to use TaskType enum and add TaskType class

--- DTQ/src/main/java/com/e1/DTQ/model/TaskRequest.java
+++ DTQ/src/main/java/com/e1/DTQ/model/TaskRequest.java
@@ -7,14 +7,14 @@ import java.util.Map;
 public record TaskRequest (
 
     String taskId,
-    String taskType,
+    TaskType taskType,
     Map<String, Object> payload, // 2. Use Map<String, Object> for flexible payload structure
     int retryCount,
     long creatingTimestamp
 
 ) implements Serializable { // 1. Implement Serializable for potential future use in distributed systems
 
- public TaskRequest( String taskId, String taskType, Map<String, Object> payload) {
+ public TaskRequest( String taskId, TaskType taskType, Map<String, Object> payload) {
         this(taskId, taskType, payload, 0, Instant.now().toEpochMilli());
     }
 
---
+++ DTQ/src/main/java/com/e1/DTQ/model/TaskType.java
@@ -0,0 +1,7 @@
+package com.e1.DTQ.model;
+
+public enum TaskType {
+    HEAVY_TASK, // Heavy tasks that require more resources and time. to prove the system can handle different types of tasks and prioritize them accordingly.
+    LIGHT_TASK // Light tasks that are quick to execute and require fewer resources. to demonstrate the system's ability to manage and execute tasks of varying complexity and resource requirements.
+}
+
